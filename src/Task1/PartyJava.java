a9dc56adce1ce38cbb3b80505ff66eabe20ccfab
FantinDmitry <65277039+FantinDmitry@users.noreply.github.com>
2020-05-20 12:17:14 +0300

    Update PartyJava.java

--- src/Task1/PartyJava.java
+++ src/Task1/PartyJava.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class PartyJava {
     public static void main(String[] args) {
         Frame f = new Frame();
-        Label I = new Label("Вечеринка у Тима");
+        Label I = new Label("Вечеринка у Игоря");
         Button b = new Button("Ваша ставка");
         Button c = new Button("Сбросить");
         Panel p = new Panel();
