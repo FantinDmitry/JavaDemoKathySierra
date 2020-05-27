89a40ac482c9af82cb987d99ae2de47502544865
FantinDmitry <fdv.77@mail.ru>
2020-05-27 21:50:44 +0300

    Logika

---
+++ src/Task/PartyJava.java
@@ -0,0 +1,17 @@
+package Task;
+
+import java.awt.*;
+
+public class PartyJava {
+    public static void main(String[] args) {
+        Frame f = new Frame();
+        Label I = new Label("Вечеринка у Игоря");
+        Button b = new Button("Ваша ставка");
+        Button c = new Button("Сбросить");
+        Panel p = new Panel();
+        p.add(I);
+
+        // Ещё код...
+
+    }
+}
